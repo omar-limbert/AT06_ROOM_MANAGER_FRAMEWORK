0b7b6a67200ac4f7273fae12e5a231b04accf441
Daniel Caballero <danielditer@gmail.com>
2018-07-16 15:26:14 -0400

    Fixed Request manager

--- core/modules/request_manager.py
+++ core/modules/request_manager.py
@@ -1,88 +1,29 @@
 import requests
 
-headers = {'Accept': 'application/json'}
 
+# Class that performs API requests.
+class RequestManager:
+    '''
+    Builds an url for performing an API request.
+    @:param base_url: The base url for append the endpoint.
+    @:param endpoint: The endpoint.
+    @:param item_id: Id of the element.
+    '''
 
-# Method for build url
-def build_uri(base_url, end_point):
-    uri = base_url + end_point
-    return uri
+    @staticmethod
+    def build_url(base_url, endpoint, item_id=''):
+        return "{}/{}/{}".format(base_url, endpoint, item_id)
 
-# Performs a request with GET method.
-def get_request(base_url, end_point, credentials, param):
-    uri = build_uri(base_url, end_point)
-    response = requests.get(url=uri, headers=headers, params=param)
-    return response
+    '''
+    Performs an API request.
+    @:param method: The method.
+    @:param url: The comlete URL.
+    @:param headers: Dictionary of HTTP Headers to send.
+    @:param params: Dictionary or bytes to be sent in the query string.
+    @:param body: A JSON serializable Python object to send in the body of the request.
+    '''
 
-# Performs a request with POST method.
-def post_request(base_url, end_point, credentials, data):
-    uri = build_uri(base_url,end_point)
-    response = requests.post(url=uri, headers=headers, json=data)
-    return response
-
-# Performs a request with PUT method.
-def put_request(base_url, end_point, credentials, data):
-    uri = build_uri(base_url, end_point)
-    response = requests.put(url=uri, headers=headers, json=data)
-    return response
-
-# Performs a request with DELETE method.
-def delete_request(base_url, end_point, credentials, params):
-    uri = build_uri(base_url, end_point)
-    response = requests.delete(url=uri, headers=headers, params=params)
-    return response
-
-# Performs a request with GET or DELETE method.
-def get_delete_request(base_url, end_point, method, credentials, item_id, params):
-    response = None
-    uri = build_uri(base_url, end_point)
-    if item_id is not None:
-        headers['Credentials'] = credentials
-        headers['ServiceName'] = 'ExchangeServer'
-        uri = "{}/{}".format(uri, item_id)
-        if method == 'GET':
-            response = requests.get(url=uri, headers=headers, params=params)
-        elif method == 'DELETE':
-            response = requests.delete(url=uri, headers=headers, params=params)
-    elif method == 'GET':
-        if credentials is not None:
-            headers['Credentials'] = credentials
-            response = requests.get(url=uri, headers=headers, params=params)
-        else:
-            response = requests.get(url=uri, headers=headers, params=params)
-    return response
-
-# Performs a request with POST or PUT method.
-def post_put_request(base_url, end_point, method, credentials, item_id, data):
-    response = None
-    uri = build_uri(base_url, end_point)
-    headers['Content-Type'] = 'application/json'
-    headers['Credentials'] = credentials
-    if item_id is not None:
-        headers['ServiceName'] = 'ExchangeServer'
-        uri = "{}/{}".format(uri, item_id)
-        if method == 'POST':
-            response = requests.post(url=uri, headers=headers, json=data)
-        elif method == 'PUT':
-            response = requests.put(url=uri, headers=headers, json=data)
-    elif method == 'POST':
-        response = requests.post(url=uri, headers=headers, json=data)
-    return response
-
-# Executes a request.
-def request(base_url, endpoint, method, credentials, item_id, data, params):
-    headers['ServiceName'] = 'ExchangeServer'
-    if method == 'GET':
-        return get_delete_request(base_url, endpoint, method, credentials, item_id, params)
-    elif method == 'POST':
-        headers['Content-Type'] = 'application/json'
-        headers['Credentials'] = credentials
-        return post_request(base_url, endpoint, credentials, data)
-    elif method == 'PUT':
-        headers['Content-Type'] = 'application/json'
-        return put_request(base_url, endpoint, credentials, data)
-    elif method == 'DELETE':
-        return delete_request(base_url, endpoint, credentials, data)
-
-
-
+    @staticmethod
+    def execute_request(method, url, headers={},
+                        params={}, body={}):
+        return requests.request(method, url, headers=headers, params=params, json=body).json()
