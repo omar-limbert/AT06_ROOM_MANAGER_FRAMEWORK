46c2d7df41747e7648a30baa55242d7331ee1226
JimmyRomero <jimmy.romero@fundacion-jala.org>
2018-07-16 17:08:50 -0400

    quit duplicated code

--- core/modules/build_expected_response.py
+++ core/modules/build_expected_response.py
@@ -1,6 +1,3 @@
-from bson import ObjectId
-
-
 # Class to build the expected response.
 class BuildResponse:
 
@@ -25,36 +22,3 @@ class BuildResponse:
             return res_dict_build
         except KeyError:
             print("Build expected response: Key error")
-
-    @staticmethod
-    def is_json_request_equal_to_json_response(json_request, json_response):
-        """
-        Function to compare whether two json files are equals or not;
-        if not returns which ones are not equals.
-
-        :param json_request: is the json parameter that is sent in the request.
-        :param json_response: is the json response received after doing the request.
-        :return: a boolean value depending whether the json data are equals or not
-        """
-        if type(json_request) and type(json_response) is not dict:
-            return None
-        result = True
-        for key in json_request:
-            if key in json_response:
-                if type(json_request[key]) is ObjectId:
-                    if str(json_request[key]) == str(json_response[key]):
-                        continue
-                    else:
-                        result = False
-                        print("JSON_request: {} >>> ObjId({}) not equals to JSON_response: {} >>> ObjId({})"
-                              .format(key, json_request[key], key, json_response[key]))
-                else:
-                    if json_request[key] == json_response[key]:
-                        # print("JSON_request: {} >>> ObjId({}) is equals to JSON_response: {} >>> ObjId({})"
-                        #       .format(key, json_request[key], key, json_response[key]))
-                        continue
-                    else:
-                        print("JSON_request: {} >>> ObjId({}) not equals to JSON_response: {} >>> ObjId({})".
-                              format(key, json_request[key], key, json_response[key]))
-                        result = False
-        return result
