443bf8301b00fee72062796bc6605cf0388bb7ff
Chritian92 <35768795+Chritian92@users.noreply.github.com>
2018-07-17 14:49:46 -0400

    add get
    *add function of get items

--- core/modules/db_manager.py
+++ core/modules/db_manager.py
@@ -1,13 +1,18 @@
+from bson.objectid import ObjectId
 from core.utils.singleton_db_connection import SingletonMongoClient
 
 
 class DataBaseManager:
 
     @staticmethod
-    def get_items():
-        get_connection = SingletonMongoClient().get_connection()
-        data_base = get_connection["rmserver"]
-        return data_base
+    def get_all_items(post_id):
+        client = SingletonMongoClient().get_connection()
+        data_base = client["rmserver"]
+        if data_base is not None:
+            result = client["rmserver"].collection.find_one({'_id': ObjectId(post_id)})
+        else:
+            result = client["rmserver"].collection.find_one({'_id'})
+        return result
 
     @staticmethod
     def remove_docs_from_collection_db(host, port, data_base_name, collection):
@@ -18,6 +23,6 @@ class DataBaseManager:
 
 
 if __name__ == "__main__":
-    is_equal = DataBaseManager.get_items()
+    is_equal = DataBaseManager.get_all_items('478521365985478542693652')
     print(is_equal)
 
