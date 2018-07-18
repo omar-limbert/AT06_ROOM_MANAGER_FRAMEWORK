4f2e8c3e8360105f9cc43d3fe58a78933c40ebd2
Daniel Caballero <danielditer@gmail.com>
2018-07-18 00:41:20 -0400

    Fixed Request Manager

--- core/modules/request_manager.py
+++ core/modules/request_manager.py
@@ -4,40 +4,47 @@ import requests
 # Class that performs API requests.
 class RequestManager:
 
-    @staticmethod
-    def build_url(base_url, endpoint, item_id=''):
-        '''
-            Builds an url for performing an API request.
-            @:param base_url: The base url for append the endpoint.
-            @:param endpoint: The endpoint.
-            @:param item_id: Id of the element.
-            '''
-        return "{}/{}/{}".format(base_url, endpoint, item_id)
+    def __init__(self, headers, params, body):
+        self.headers = headers
+        self.params = params
+        self.body = body
+
+    def set_headers(self, headers):
+        self.headers = headers
+
+    def set_params(self, params):
+        self.params = params
+
+    def set_body(self, body):
+        self.body = body
 
     @staticmethod
-    def add_header(key, value, headers):
-        '''
-            Add a header to the dictionary of headers.
-           @:param key: the key of the element.
-           @:param value: the value of the key.
-           @:param headers: dictionary of headers.
-           '''
-        headers[key] = value
+    def build_url(base_url, endpoint, item_id):
+        if item_id is not None:
+            return "{}/{}/{}".format(base_url, endpoint, item_id)
+        else:
+            return "{}/{}".format(base_url, endpoint)
 
-    @staticmethod
-    def execute_request(method, base_url, endpoint, item_id='', headers={}, params={}, body={}):
+    def execute_request(self, method, base_url, endpoint, item_id):
         '''
             Performs an API request.
             @:param method: The method.
             @:param base_url: The base URL.
             @:param endpoint: The endpoint.
             @:param item_id: Id of the item for perform the request.
-            @:param headers: Dictionary of HTTP Headers to send.
-            @:param params: Dictionary or bytes to be sent in the query string.
-            @:param body: A JSON serializable Python object to send in the body of the request.
             '''
-        url = RequestManager.build_url(base_url, endpoint, item_id)
-        if method == 'GET' or method == 'DELETE':
-            return requests.request(method, url, headers=headers, params=params)
-        elif method == 'POST' or method == 'PUT':
-            return requests.request(method, url, headers=headers, json=body)
+        uri = RequestManager.build_url(base_url, endpoint, item_id)
+        if self.headers is not None:
+            if method == 'DELETE':
+                return requests.delete(uri, headers=self.headers)
+            elif self.params is not None:
+                if method == 'GET':
+                    return requests.get(uri, headers=self.headers, params=self.params)
+            elif self.body is not None:
+                if method == 'POST' or method == 'PUT':
+                    return requests.request(method, uri, headers=self.headers, json=self.body)
+        else:
+            if method == 'GET':
+                return requests.get(uri)
+            elif method == 'POST':
+                return requests.post(uri, json=self.body)
